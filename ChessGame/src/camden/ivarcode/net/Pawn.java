96f912eeecfaa22915f36eb6fcf7b9ad386c2405
Camden Wagner <camden.i.wagner@ivarcode.net>
2015-10-27 00:03:10 -0400

    implementing more methods in the Location and Piece class

---
+++ ChessGame/src/camden/ivarcode/net/Pawn.java
@@ -0,0 +1,13 @@
+package camden.ivarcode.net;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class Pawn extends Piece {
+
+	public Pawn(Location loc, String color, BufferedImage img, Graphics g) {
+		super(loc, color, img, g);
+		// TODO Auto-generated constructor stub
+	}
+
+}
