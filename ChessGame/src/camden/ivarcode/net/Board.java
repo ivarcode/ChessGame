4e9e56157c3247405b2d616b62dddf4cc1d4e9da
Camden Wagner <camden.i.wagner@ivarcode.net>
2015-11-17 16:37:25 -0500

    constructing new Board

--- ChessGame/src/camden/ivarcode/net/Board.java
+++ ChessGame/src/camden/ivarcode/net/Board.java
@@ -1,24 +1,11 @@
 package camden.ivarcode.net;
 
-import java.awt.Graphics;
-import java.awt.Image;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-
-import javax.imageio.ImageIO;
-
-import camden.ivarcode.net.piece.Bishop;
-import camden.ivarcode.net.piece.King;
-import camden.ivarcode.net.piece.Knight;
-import camden.ivarcode.net.piece.Pawn;
-import camden.ivarcode.net.piece.Queen;
-import camden.ivarcode.net.piece.Rook;
-
 public class Board {
 
 
-	
+	public Board(Piece[] pieces) {
+		
+	}
 	
 
 }
